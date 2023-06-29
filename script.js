61f9dbd45c8adbaf612e6a28e80d54931e3a1a1e
SirajAtFiverr <sirajatfiverr@gmail.com>
2023-06-29 09:53:38 +0500

    save all changes: icons added and manifest file added

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
 
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
 const endorsementTextField = document.querySelector(".endorsement-text-field");
 const fromField = document.querySelector(".from-field");
 const toField = document.querySelector(".to-field");
 const publishButton = document.querySelector(".publish-button");
-
 const endorsementListEl = document.querySelector(".endorsement-list");
 
-const appSettings = {
-    databaseURL: "https://we-are-champion-80ba1-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+// ############################################ DATABASE SETTING ############################################
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGhIKaqR50P1d6N0aqUX76F0XSFmbc_lA",
+    authDomain: "we-are-champion-80ba1.firebaseapp.com",
+    databaseURL: "https://we-are-champion-80ba1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "we-are-champion-80ba1",
+    storageBucket: "we-are-champion-80ba1.appspot.com",
+    messagingSenderId: "662309222812",
+    appId: "1:662309222812:web:f054723f9ac6592957b059"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const dbRef = firebase.database().ref();
+const emdorsementsInDB = dbRef.child("emdorsements")
+
+// ########################################### DATABASE SETTING END ##########################################
 
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app)
-const emdorsementsInDB = ref(database, "emdorsements");
 
 
-let endorsements = [
+// ############################################ DATABASE FUNCTIONS ############################################
 
-    {
-        "to": "Leanne",
-        "from": "Sindre",
-        "text": "Leanne! Thank you so much for helping me with the March accounting. \
-                 Saved so much time because of you! 💜 Frode",
-        "likes": 7
-    },
-
-    {
-        "to": "Bob",
-        "from": "Per",
-        "text": "Hi Bob! Your React Router course is so good. \
-                The students are going to LOVE IT. I’m so excited for the launch :) 🔥 Per",
-        "likes": 10
-    },
-
-    {
-        "to": "Abdellah",
-        "from": "Sindre",
-        "text": "That transcription feature you completed for Scrimba 3.0 is amazing. \
-                I know you’ve been working hard on it for several months now. 👏👏👏 Really good work 🙌",
-        "likes": 9
+function addToDB(endorsement, to, from, likes){
+    let endorsementObj =  {
+        "to": to,
+        "from": from,
+        "text": endorsement,
+        "likes": likes
     }
 
-];
+    emdorsementsInDB.push(endorsementObj);
+}
+
+emdorsementsInDB.on("value", function(snapshot){
+    if(!snapshot.exists()) return
+
+    renderEndorsements(Object.entries(snapshot.val()));
+    listenForLikes();
+});
+
+function updateLikes(key, newLikes){
+    const updates = {
+        likes: newLikes
+    }
+    emdorsementsInDB.child(key).update(updates);
+}
+
+// ########################################### DATABASE FUNCTIONS END ##########################################
+
+
+
+
+// ###################################### WHEN PUSH BUTTON IS CLICKED ######################################
 
 publishButton.addEventListener("click", function(){
     let endorsement = endorsementTextField.value
@@ -58,39 +71,50 @@ publishButton.addEventListener("click", function(){
 
     addToDB(endorsement, to, from, 0);
     clearFields();
-})
+});
 
-function addToDB(endorsement, to, from, likes){
-    let endorsementObj =  {
-        "to": to,
-        "from": from,
-        "text": endorsement,
-        "likes": likes
-    }
+// ##################################### WHEN PUSH BUTTON IS CLICKED END ####################################
 
-    push(emdorsementsInDB, JSON.stringify(endorsementObj));
+
+
+
+// ########################################### CLEARING FUNCTIONS ###########################################
+
+function clearFields(){
+    endorsementTextField.value = "";
+    toField.value = "";
+    fromField.value = "";
 }
 
 function clearEndorsementList(){
     endorsementListEl.innerHTML = "";
 }
 
+// ########################################## CLEARING FUNCTIONS END #########################################
+
+
+
+
+// ###################################### ENDORSEMENT RENDRING FUNCTIONS #####################################
+
 function renderEndorsements(endorsements){
+
     clearEndorsementList();
-    for(let i=0; i<endorsements.length; i++){
-            console.log(endorsements[i])
-            appendEndorsement(endorsements[i]);
+    for(let i=endorsements.length-1; i>=0; i--){
+            renderEndorsement(endorsements[i]);
     }
 }
 
-function appendEndorsement(endorsementKeyValuePair){
-    let key = endorsementKeyValuePair[0];
-    let endorsement = JSON.parse(endorsementKeyValuePair[1]);
+function renderEndorsement(endorsementKeyValuePair){
+    const key = endorsementKeyValuePair[0];
+    const endorsement = endorsementKeyValuePair[1];
+
     let heartIcon = '🖤';
+    if(isLiked(key))  heartIcon = '❤️';
 
    let endorsementListItem =  `
     <li>
-        <divclass ="to">To ${endorsement.to}</div>
+        <div class ="to">To ${endorsement.to}</div>
         <div class="endorsement-text">
             ${endorsement.text}
         </div>
@@ -100,17 +124,49 @@ function appendEndorsement(endorsementKeyValuePair){
         </div>
     </li>
     `
-
     endorsementListEl.innerHTML += endorsementListItem;
 }
 
-function clearFields(){
-    endorsementTextField.value = "";
-    toField.value = "";
-    fromField.value = "";
+// #################################### ENDORSEMENT RENDRING FUNCTIONS END ####################################
+
+
+
+
+// ############################################## LIKES FUNCTIONS #############################################
+
+function listenForLikes(){
+    const likes = document.querySelectorAll(".likes")
+    likes.forEach(like => {
+        like.addEventListener("click", function(event){
+            const key = event.target.id;
+            const previousLikes = Number(event.target.textContent.split(" ")[1]);
+
+            let newLikes = previousLikes;
+            if(isLiked(key)){
+                removeLike(key);
+                newLikes -= 1;
+            } 
+            else{
+                addLike(key);
+                newLikes += 1;
+            }      
+
+            updateLikes(event.target.id, newLikes);
+        });
+    });
 }
 
-onValue(emdorsementsInDB, function(snapshot){
-    if(!snapshot.exists()) return
-    renderEndorsements(Object.entries(snapshot.val()));
-});
+function addLike(key){
+    localStorage.setItem(key, "liked");
+}
+
+function removeLike(key){
+    localStorage.removeItem(key);
+}
+
+function isLiked(key){
+    if(localStorage.getItem(key)) return true
+    return false;
+}
+
+// ############################################ LIKES FUNCTIONS END ############################################
